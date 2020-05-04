a331c3f48909926106f4e666676db17809736304
unknown <vodili321@gmail.com>
2020-05-04 18:47:26 -0400

    adding a change from the master branch

---
+++ v2.1.1/simple-app/src/main/java/com/xyz/App.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *
+ ******************************************************************************/
+package com.xyz;
+
+public class App {
+
+        public static void main(String[] args) {
+                System.out.println("Hello World!");
+        }
+
+}
